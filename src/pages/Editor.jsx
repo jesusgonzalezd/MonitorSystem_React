844901f8c8b7c222c7be6b9e415419a4a251862e
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-07-05 12:23:24 -0400

    Editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -4,13 +4,27 @@ import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent,
 import  HeaderTable  from '../components/header/HeaderTable';
 import { EditorData } from '../data/dummy';
 
-const Editor = () => (
+const Editor = () => {
+
+  const toolbarOptions = {items:['Bold', 'Italic', 'Underline', 'StrikeThrough',
+  'FontName', 'FontSize', 'FontColor', 'BackgroundColor',
+  'LowerCase', 'UpperCase', '|',
+  'Formats', 'Alignments', 'OrderedList', 'UnorderedList',
+  'Outdent', 'Indent', '|',
+  'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
+  'SourceCode', '|', 'Undo', 'Redo'
+]};
+
+  return(
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <HeaderTable category="App" title="Editor" />
-    <RichTextEditorComponent>
+    <RichTextEditorComponent
+     toolbarSettings={toolbarOptions}
+     >
       <EditorData />
       <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
     </RichTextEditorComponent>
   </div>
-);
+  );
+};
 export default Editor;
